d89aa1bbbce019cedaca24fc712a981f2ea656ab
Van Dong <thenullmastermind@gmail.com>
2024-02-17 20:41:20 +0700

    Update DLL path in 'getDll' method
    Adjusted the 'getDll' method in util.ts to point to the new DLL directory instead of the original library directory. This modification is related to the file organizational changes, specifically concerning DLL files.

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@ import * as os from "os";
 import path from "node:path";
 
 export const getDll = () => {
-  const libDir = path.join(__dirname, "../lib");
+  const libDir = path.join(__dirname, "../dlls");
 
   switch (os.arch()) {
     case "ia32":
