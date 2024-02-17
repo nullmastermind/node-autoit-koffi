987962a60eff635e3f7a7112d981c5680abd1303
Van Dong <thenullmastermind@gmail.com>
2024-02-17 20:56:02 +0700

    Implement koffi library in index.ts file
    Added the koffi library to the index.ts file and updated the "getDll" function to support it. This change throws an error if the DLL is not supported by the OS and exports an 'init' function that returns a promise for asynchronous operation.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 import { getDll } from "./util";
+import * as koffi from "koffi";
 
-console.log(getDll());
+const dll = getDll();
+if (!dll) throw new Error("This operating system is not supported!");
+const lib = koffi.load(dll);
+const fn = {
+  init: lib.func("AU3_Init", "void", []),
+};
+
+export const init = (): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    fn.init.async((err: Error, res: any) => {
+      if (err) reject(err);
+      else resolve(res);
+    });
+  });
+};
+
+init().then(console.log).catch(console.error);
